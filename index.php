b9f3d672426cbe5e09264cce47bbfcefc8a25637
QuintansC <gustavosantos.2095@gmail.com>
2021-05-06 12:27:22 -0300

    Especificando caminho

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
     <body>
         <div class="container">
            <?php
-                include ('./pages/header.php');
-                include ('./pages/sliders.php');
-                include ('./pages/content.php');
-                include ('./pages/highlights.php');
+                include ('./src/pages/header.php');
+                include ('./src/pages/sliders.php');
+                include ('./src/pages/content.php');
+                include ('./src/pages/highlights.php');
             ?>
         <div>
-        <?php include ('./pages/footer.php'); ?>
+        <?php include ('./src/pages/footer.php'); ?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- src/pages/header.php
+++ src/pages/header.php
@@ -2,7 +2,16 @@
     <img class="logo" src="./src/images/SpaceX.svg" alt="Logo"/>
     <nav class="nav justify-content-end center">
         <a class="nav-link text-secondary" href="#">Home</a>
-        <a class="nav-link text-secondary" href="#">Empresa</a>
+        <div class="dropdown">
+            <button class="btn text-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Empresa
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item text-secondary" href="#">Item1</a>
+                <a class="dropdown-item text-secondary" href="#">Item2</a>
+                <a class="dropdown-item text-secondary" href="#">Item3</a>
+            </div>
+        </div>
         <a class="nav-link text-secondary" href="#">Clientes</a>
         <a class="nav-link text-secondary" href="#">Contato</a>
     </nav>
